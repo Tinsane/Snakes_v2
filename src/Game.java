be90a5a87f5aab253681a4888a61804e8baf76db
Владимир <icyfalcon@mail.ru>
2016-09-16 21:34:16 +0500

    Game cleared

--- src/Game.java
+++ src/Game.java
@@ -2,12 +2,4 @@
  * Created by Владимир on 16.09.2016.
  */
 public class Game {
-    private static Game ourInstance = new Game();
-
-    public static Game getInstance() {
-        return ourInstance;
-    }
-
-    private Game() {
-    }
 }
