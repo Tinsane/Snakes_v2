bb95469446ee7455d8dcc72d8005b53434ec799c
Владимир <icyfalcon@mail.ru>
2016-09-20 19:10:08 +0500

    keks

--- src/BaseMapObject.java
+++ src/BaseMapObject.java
@@ -21,7 +21,19 @@ public abstract class BaseMapObject implements MapObject
     @Override
     public void processCollision(MapObject collidedObject, Game game)
     {
-        throw new NotImplementedException();
+        Class<? extends MapObject> collidedObjectClass = collidedObject.getClass();
+        if (collidedObjectClass == SnakeCell.class)
+            this.processCollision((SnakeCell) collidedObject, game);
+        else if (collidedObjectClass == Blueberry.class)
+            this.processCollision((Blueberry) collidedObject, game);
+        else if (collidedObjectClass == Strawberry.class)
+            this.processCollision((Strawberry) collidedObject, game);
+        else if (collidedObjectClass == Wall.class)
+            this.processCollision((Wall) collidedObject, game);
+        else if (collidedObjectClass == SandGlass.class)
+            this.processCollision((SandGlass) collidedObject, game);
+        else
+            throw new NotImplementedException();
         // TODO: reflection here
     }
 }
