93c546adf7e4ec5c1ebbc9cb33c12bbed0b50739
Владимир <icyfalcon@mail.ru>
2016-09-21 11:39:26 +0500

    GameUpdater added

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -18,10 +18,9 @@ public class Game
 {
     private static final int DEFAULT_UPDATE_DELAY = 300;
     public Snake snake;
+    private GameUpdater gameUpdater;
     private LinkedList<MapObject[][]> maps;
     private Timer gameTimer;
-    private Random gameRandom;
-
     Game(MapObject[][] map, Snake snake)
     {
         this(map, snake, DEFAULT_UPDATE_DELAY);
@@ -32,16 +31,8 @@ public class Game
         maps = new LinkedList<>();
         maps.addFirst(map);
         this.snake = snake;
-        gameRandom = new Random();
         gameTimer = new Timer(updateDelay, e -> update());
-    }
-
-    private static void clearDestructedObjects(MapObject[][] map)
-    {
-        for (int x = 0; x < map.length; ++x)
-            for (int y = 0; y < map[0].length; ++y)
-                if (map[x][y] != null && map[x][y].getIsDestructed())
-                    map[x][y] = null;
+        gameUpdater = new GameUpdater(this);
     }
 
     public void executeCommand(GameCommand command)
@@ -70,76 +61,103 @@ public class Game
         gameTimer.stop();
     }
 
-    private void generateBerry(MapObject[][] map)
-    {
-        int freeCellsCnt = 0;
-        for (MapObject[] row : map)
-            for (MapObject cell : row)
-                if (cell == null)
-                    ++freeCellsCnt;
-        if (freeCellsCnt == 0)
-            return;
-        int berryCellNumber = gameRandom.nextInt(freeCellsCnt);
-        for (int x = 0; x < map.length; ++x)
-            for (int y = 0; y < map[0].length; ++y)
-            {
-                if (berryCellNumber == 0)
-                    map[x][y] = gameRandom.nextBoolean() ? new Blueberry() : new Strawberry();
-                if (map[x][y] == null)
-                    --berryCellNumber;
-            }
-    }
-
-    private void placeObject(MapObject[][] map, MapObject object, IntPair position)
-    {
-        if (map[position.x][position.y] != null)
-            object.processCollision(map[position.x][position.y], this);
-        if (!object.getIsDestructed())
-            map[position.x][position.y] = object;
-    }
-
-    private void moveSnakeCell(MapObject[][] newMap, SnakeCell cell, IntPair cellPosition)
-    {
-        if (cell.previous != null)
-        {
-            IntPair previousPosition = SnakeCell.getPreviousCoordinates(getCurrentMap(), cellPosition);
-            if (previousPosition == null)
-                placeObject(newMap, cell.previous, cellPosition);
-            else
-                moveSnakeCell(newMap, cell.previous, previousPosition);
-            cell.previous.setVelocity(cell.getVelocity());
-        }
-        moveObject(newMap, cellPosition);
-    }
-
-    private void moveSnake(MapObject[][] newMap)
-    {
-        moveSnakeCell(newMap, snake.head, snake.head.getCoordinates(getCurrentMap()));
-    }
-
-    private void moveObject(MapObject[][] newMap, IntPair position)
-    {
-        MapObject[][] curMap = getCurrentMap();
-        MapObject curObject = curMap[position.x][position.y];
-        IntPair newPosition = position.getAdded(curObject.getVelocity().getIntPair());
-        placeObject(newMap, curObject, newPosition);
-    }
-
     public void update()
     {
-        MapObject[][] curMap = maps.peekFirst();
-        MapObject[][] newMap = new MapObject[curMap.length][curMap[0].length];
-        for (int x = 0; x < curMap.length; ++x)
-            for (int y = 0; y < curMap[0].length; ++y)
-            {
-                MapObject curObject = curMap[x][y];
-                if (!(curObject == null || curObject.getIsDestructed() || curObject.getClass() == SnakeCell.class))
-                    moveObject(newMap, new IntPair(x, y));
-            }
-        moveSnake(newMap);
-        clearDestructedObjects(newMap);
-        maps.addFirst(newMap);
+        maps.addFirst(gameUpdater.getNewMap());
         if (snake.getIsDestructed())
             stop();
+    }
+
+    private class GameUpdater
+    {
+        private Game game;
+        private Random updaterRandom;
+        private MapObject[][] newMap;
+
+        private GameUpdater(Game game)
+        {
+            this.game = game;
+            updaterRandom = new Random();
+        }
+
+        private void generateBerry()
+        {
+            int freeCellsCnt = 0;
+            for (MapObject[] row : newMap)
+                for (MapObject cell : row)
+                    if (cell == null)
+                        ++freeCellsCnt;
+            if (freeCellsCnt == 0)
+                return;
+            int berryCellNumber = updaterRandom.nextInt(freeCellsCnt);
+            for (int x = 0; x < newMap.length; ++x)
+                for (int y = 0; y < newMap[0].length; ++y)
+                {
+                    if (berryCellNumber == 0)
+                        newMap[x][y] = updaterRandom.nextBoolean() ? new Blueberry() : new Strawberry();
+                    if (newMap[x][y] == null)
+                        --berryCellNumber;
+                }
+        }
+
+        private void clearDestructedObjects()
+        {
+            for (int x = 0; x < newMap.length; ++x)
+                for (int y = 0; y < newMap[0].length; ++y)
+                    if (newMap[x][y] != null && newMap[x][y].getIsDestructed())
+                        newMap[x][y] = null;
+        }
+
+        private void placeObject(MapObject object, IntPair position)
+        {
+            if (newMap[position.x][position.y] != null)
+                object.processCollision(newMap[position.x][position.y], game);
+            if (!object.getIsDestructed())
+                newMap[position.x][position.y] = object;
+        }
+
+        private void moveSnakeCell(SnakeCell cell, IntPair cellPosition)
+        {
+            if (cell.previous != null)
+            {
+                IntPair previousPosition = SnakeCell.getPreviousCoordinates(game.getCurrentMap(), cellPosition);
+                if (previousPosition == null)
+                    placeObject(cell.previous, cellPosition);
+                else
+                    moveSnakeCell(cell.previous, previousPosition);
+                cell.previous.setVelocity(cell.getVelocity());
+            }
+            moveObject(cellPosition);
+        }
+
+        private void moveSnake()
+        {
+            SnakeCell snakeHead = game.snake.head;
+            moveSnakeCell(snakeHead, snakeHead.getCoordinates(getCurrentMap()));
+        }
+
+        private void moveObject(IntPair position)
+        {
+            MapObject[][] curMap = getCurrentMap();
+            MapObject curObject = curMap[position.x][position.y];
+            IntPair newPosition = position.getAdded(curObject.getVelocity().getIntPair());
+            placeObject(curObject, newPosition);
+        }
+
+        private MapObject[][] getNewMap()
+        {
+            MapObject[][] curMap = game.getCurrentMap();
+            newMap = new MapObject[curMap.length][curMap[0].length];
+            for (int x = 0; x < curMap.length; ++x)
+                for (int y = 0; y < curMap[0].length; ++y)
+                {
+                    MapObject curObject = curMap[x][y];
+                    if (!(curObject == null || curObject.getIsDestructed() || curObject.getClass() == SnakeCell.class))
+                        moveObject(new IntPair(x, y));
+                }
+            moveSnake();
+            clearDestructedObjects();
+            return newMap;
+        }
     }
 }
