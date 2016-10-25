8c91f5d4762c3e72b21e8aa60239f724200ff753
Владимир <icyfalcon@mail.ru>
2016-10-25 08:32:33 +0500

    little score view improvement

--- src/Views/FinalScoreView/Canvas.java
+++ src/Views/FinalScoreView/Canvas.java
@@ -21,8 +21,11 @@ public class Canvas extends JPanel
         g2d.fill(getVisibleRect());
         g2d.setColor(Color.YELLOW);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setFont(new Font("Tahoma", Font.PLAIN, 40));
-        g2d.drawString("Score: " + score, getVisibleRect().x + getVisibleRect().width / 2 - 80,
-                getVisibleRect().y + getVisibleRect().height / 2);
+        Font font = new Font("Tahoma", Font.PLAIN, 40);
+        FontMetrics metrics = g2d.getFontMetrics(font);
+        g2d.setFont(font);
+        String text = "Score: " + score;
+        g2d.drawString(text, (getVisibleRect().width - metrics.stringWidth(text)) / 2,
+                (getVisibleRect().height - metrics.getHeight()) / 2 + metrics.getAscent());
     }
 }
