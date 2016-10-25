4be8ca9c00e013db2515990feb9e23a734ce077d
Владимир <icyfalcon@mail.ru>
2016-10-25 20:39:27 +0500

    TextButton changes color on pointing

--- src/Views/Utils/TextButton.java
+++ src/Views/Utils/TextButton.java
@@ -2,6 +2,8 @@ package Views.Utils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 /**
  * Created by Владимир on 20.10.2016.
@@ -10,24 +12,57 @@ public class TextButton extends JButton
 {
     private final String text;
     private final Font font;
-    private final Color focusedColor;
-    private final Color nonFocusedColor;
+    private final Color pointedColor;
+    private final Color regularColor;
+    private boolean isPointed;
     public TextButton(String text, Font font)
     {
         this(text, font, Color.black, Color.gray);
     }
-    public TextButton(String text, Font font, Color nonFocusedColor, Color focusedColor)
+    public TextButton(String text, Font font, Color regularColor, Color pointedColor)
     {
         super();
         this.text = text;
         this.font = font;
         this.setBorder(null);
         this.setContentAreaFilled(false);
-        this.focusedColor = focusedColor;
-        this.nonFocusedColor = nonFocusedColor;
+        this.pointedColor = pointedColor;
+        this.regularColor = regularColor;
         setBorder(null);
         setContentAreaFilled(false);
         setFocusPainted(false);
+        isPointed = false;
+        addMouseListener(new MouseListener()
+        {
+            @Override
+            public void mouseClicked(MouseEvent e)
+            {
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e)
+            {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e)
+            {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e)
+            {
+                isPointed = true;
+                repaint();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e)
+            {
+                isPointed = false;
+                repaint();
+            }
+        });
     }
     @Override
     public void paintComponent(Graphics g)
@@ -36,10 +71,10 @@ public class TextButton extends JButton
         Graphics2D g2d = (Graphics2D) g;
         FontMetrics metrics = g2d.getFontMetrics(font);
         g2d.setFont(font);
-        if (hasFocus())
-            g2d.setColor(focusedColor);
+        if (isPointed)
+            g2d.setColor(pointedColor);
         else
-            g2d.setColor(nonFocusedColor);
+            g2d.setColor(regularColor);
         g2d.drawString(text, (getVisibleRect().width - metrics.stringWidth(text)) / 2,
                 (getVisibleRect().height - metrics.getHeight()) / 2 + metrics.getAscent());
     }
